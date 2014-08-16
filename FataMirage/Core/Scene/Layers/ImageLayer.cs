9d05ece792424203f35e1be1ecc44809bb02bf9e
git <jal@jalfm.de>
2014-08-16 14:20:07 +0200

    Added some XML documentation

--- FataMirage/Core/Scene/Layers/ImageLayer.cs
+++ FataMirage/Core/Scene/Layers/ImageLayer.cs
@@ -7,38 +7,78 @@ using System.Threading.Tasks;
 
 namespace FataMirage.Core.Scene.Layers
 {
+    /// <summary>
+    /// This layer displays a static image with the size of the window at a given opacity
+    /// </summary>
     class ImageLayer : ILayer
     {
+        /// <summary>
+        /// The texture the ImageLayer will display
+        /// </summary>
         Graphics.Texture texture;
+        /// <summary>
+        /// Weather the layer is visible (this will use opacity to get and set values)
+        /// </summary>
         public bool visible;
+        /// <summary>
+        /// How opaque the layer is (0 = invisible, 1 = completely opaque)
+        /// </summary>
         public float opacity;
+        /// <summary>
+        /// Creates a new image layer that is fully visible
+        /// </summary>
+        /// <param name="texture">The texture the layer should display</param>
+        /// <param name="depth">The z-position of the layer</param>
         public ImageLayer(Graphics.Texture texture, float depth)
         {
             this._zPos = depth;
             this.texture = texture;
-            this.visible = true;
             this.opacity = 1;
         }
+        /// <summary>
+        /// Created a new image layer and loads the texture at the given location
+        /// </summary>
+        /// <param name="fileName">The name of the texture the layer should display</param>
+        /// <param name="depth">The z-position of the layer</param>
         public ImageLayer(string fileName, float depth)
         {
             this._zPos = depth;
             this.texture = new Graphics.Texture(fileName);
             this.opacity = 1;
         }
+        /// <summary>
+        /// Creates a new image layer that is fully visible
+        /// </summary>
+        /// <param name="texture">The texture the layer should display</param>
+        /// <param name="depth">The z-position of the layer</param>
+        /// <param name="opacity">The opacity of the layer</param>
         public ImageLayer(Graphics.Texture texture, float depth, float opacity)
         {
             this._zPos = depth;
             this.texture = texture;
-            this.visible = true;
             this.opacity = opacity;
         }
+        /// <summary>
+        /// Created a new image layer and loads the texture at the given location
+        /// </summary>
+        /// <param name="fileName">The name of the texture the layer should display</param>
+        /// <param name="depth">The z-position of the layer</param>
+        /// <param name="opacity">The opacity of the layer</param>
         public ImageLayer(string fileName, float depth, float opacity)
         {
             this._zPos = depth;
             this.texture = new Graphics.Texture(fileName);
             this.opacity = opacity;
         }
+        /// <summary>
+        /// Exposes the update method
+        /// </summary>
+        /// <param name="elapsedTime">The time in seconds since the last time this method was called (= the last frame)</param>
         void ILayer.Update(float elapsedTime) { _update(elapsedTime); }
+        /// <summary>
+        /// Exposes the draw method
+        /// </summary>
+        /// <param name="elapsedTime">The time in seconds since the last time this method was called (= the last frame)</param>
         void ILayer.Draw(float elapsedTime) { _draw(elapsedTime); }
         bool ILayer.collisionDetect(float x, float y){return _collisionDetect(x, y);}
         float ILayer.zPos { get { return _zPos; } set { _zPos = value; } }
