413f068fb714c239ca1b650c3f7c19c219a5163f
git <jal@jalfm.de>
2014-08-17 23:36:21 +0200

    XML-documented Item.cs and Items.cs
    I want to keep the documentation as good as possible, so this was a
    logical step

--- FataMirage/Core/Player/Inventory/Item.cs
+++ FataMirage/Core/Player/Inventory/Item.cs
@@ -12,13 +12,30 @@ namespace FataMirage.Core.Player.Inventory
     /// </summary>
     class Item
     {
+        /// <summary>
+        /// Creates a new item with the given position and texture
+        /// </summary>
+        /// <param name="initialPosition">The position. This may be overwritten when the item is actually spawned and therefore can easily be (0,0)</param>
+        /// <param name="texture">The texture of the item</param>
         public Item(Vector2 initialPosition, Graphics.Texture texture)
         {
             this.initialPosition = initialPosition;
             this.texture = texture;
         }
+
+        /// <summary>
+        /// The position where the item was spawned in the scene
+        /// </summary>
+        /// 
         public Vector2 initialPosition;
+        /// <summary>
+        /// The position towards the item is currently moving
+        /// </summary>
         public Vector2 currentGoal;
+
+        /// <summary>
+        /// Calculates the current position of the item, based on initialPosition, currentGoal and progress
+        /// </summary>
         public Vector2 currentPosition
         {
             get
@@ -27,6 +44,10 @@ namespace FataMirage.Core.Player.Inventory
                     currentGoal * progress;
             }
         }
+
+        /// <summary>
+        /// The progress of the item towards it's goal. This is the liearProgress, but processed by the smooth step algorithm
+        /// </summary>
         public float progress
         {
             get
@@ -36,11 +57,30 @@ namespace FataMirage.Core.Player.Inventory
                     2 * Math.Pow(linearProgress, 3));
             }
         }
+
+        /// <summary>
+        /// The linear progress of the item towards its goal
+        /// </summary>
         public float linearProgress;
+
+        /// <summary>
+        /// The texture of the item
+        /// </summary>
         public Graphics.Texture texture;
+
         //TODO: Find a better name!
+        /// <summary>
+        /// Weather the item is visible in the scene
+        /// </summary>
         public bool OnStage;
 
+        /// <summary>
+        /// Updates the item and moves it towards its goal
+        /// </summary>
+        /// <param name="elapsedTime">The time since this method was called the last time, emasured in seconds</param>
+        /// <param name="inInventoryPosition">The position of the item in the inventory. 
+        /// This will be set as the currentGoal of the item. 
+        /// Changing this while the item is still moving might result in weird results</param>
         public void Update(float elapsedTime, Vector2 inInventoryPosition)
         {
             if (OnStage)
--- FataMirage/Core/Player/Inventory/Items.cs
+++ FataMirage/Core/Player/Inventory/Items.cs
@@ -9,12 +9,22 @@ namespace FataMirage.Core.Player.Inventory
 {
     static class Items
     {
+        /// <summary>
+        /// A list of all items avilable in the game, containting also the items that are currently not in the players inventory
+        /// </summary>
         public static Dictionary<string, Item> items = new Dictionary<string, Item>();
+        /// <summary>
+        /// Updates all items
+        /// </summary>
+        /// <param name="elapsedTime">The time since the last time this methdo was called, measured in seconds</param>
         public static void Update(float elapsedTime)
         {
             for (int i = 0; i < items.Count();i++ )
                 items.ElementAt(i).Value.Update(elapsedTime, new Vector2(InventoryConfig.getBounds(i).X , 0));
         }
+        /// <summary>
+        /// The number of items that exist in total
+        /// </summary>
         public static int count
         {
             get
@@ -22,6 +32,11 @@ namespace FataMirage.Core.Player.Inventory
                 return items.Count;
             }
         }
+        /// <summary>
+        /// Returns the position of the item at the given index in the item list (not the inventory)
+        /// </summary>
+        /// <param name="index">The index in the item list</param>
+        /// <returns>If the item is visible, its bounds, otherwise, a new RectangleF(0,0,0,0)</returns>
         public static SharpDX.RectangleF getPosition(int index)
         {
             if (!items.ElementAt(index).Value.OnStage)
@@ -34,6 +49,11 @@ namespace FataMirage.Core.Player.Inventory
                 InventoryConfig.height,
                 InventoryConfig.height);
         }
+        /// <summary>
+        /// Returns the texture of the item at the given index in the item list (not the inventory)
+        /// </summary>
+        /// <param name="index">The index in the item list</param>
+        /// <returns>The texture of the item</returns>
         public static Texture2D getTexture(int index)
         {
             return items.ElementAt(index).Value.texture.texture;
