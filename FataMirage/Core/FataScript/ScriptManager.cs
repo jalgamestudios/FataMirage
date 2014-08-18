6fc3c33e21dc16c60dbbb50f0159b6f2beec11a7
git <jal@jalfm.de>
2014-08-18 18:56:25 +0200

    Fixed bug in if and ifn
    I mixed up the arguments with the line, which created code that was
    impossibel to read (because it was wrong) nad, much more important,
    failed to execute the fatascript in the way the user wanted it to
    execute

--- FataMirage/Core/FataScript/ScriptManager.cs
+++ FataMirage/Core/FataScript/ScriptManager.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using System.Globalization;
+using System.Threading.Tasks;
 
 namespace FataMirage.Core.FataScript
 {
@@ -18,55 +19,84 @@ namespace FataMirage.Core.FataScript
         }
         public static void ExecuteScript(string name)
         {
-            string[] lines = scripts[name].Split(Environment.NewLine.ToCharArray());
-            List<string> linesPrecompiled = new List<string>();
-            foreach (string line in lines)
-            {
-                if (line.Trim().StartsWith("#") || line.Trim() == "")
-                    continue;
-                linesPrecompiled.Add(line.Trim());
-            }
-            for (int i = 0; i < linesPrecompiled.Count; i++)
-            {
-                List<string> arguments = linesPrecompiled[i].Split(' ').ToList();
-                string command = arguments[0];
-                arguments.RemoveAt(0);
-                if (command == "if")
+            //Makes the script run on a new thread so it can run independently of the core game
+            Task.Run(new Action(() =>
                 {
-                    if (FataSharpVarProvider.GetVar(arguments[0]) == arguments[1])
+                    string[] lines = scripts[name].Split(Environment.NewLine.ToCharArray());
+                    List<string> linesPrecompiled = new List<string>();
+                    foreach (string line in lines)
                     {
-                        while (arguments[i] != arguments[2] && i < linesPrecompiled.Count)
-                            i++;
-                        continue;
+                        if (line.Trim().StartsWith("#") || line.Trim() == "")
+                            continue;
+                        linesPrecompiled.Add(line.Trim());
                     }
-                }
-                else if (command == "ifn")
-                {
-                    if (FataSharpVarProvider.GetVar(arguments[0]) != arguments[1])
+                    for (int i = 0; i < linesPrecompiled.Count; i++)
                     {
-                        while (arguments[i] != arguments[2] && i < linesPrecompiled.Count)
-                            i++;
-                        continue;
+                        List<string> arguments = linesPrecompiled[i].Split(' ').ToList();
+                        string command = arguments[0];
+                        arguments.RemoveAt(0);
+                        if (command == "if")
+                        {
+                            if (FataSharpVarProvider.GetVar(arguments[0]) == arguments[1])
+                            {
+                                while (arguments[i] != arguments[2] && i < linesPrecompiled.Count)
+                                    i++;
+                                continue;
+                            }
+                        }
+                        else if (command == "ifn")
+                        {
+                            if (FataSharpVarProvider.GetVar(arguments[0]) != arguments[1])
+                            {
+                                while (arguments[i] != arguments[2] && i < linesPrecompiled.Count)
+                                    i++;
+                                continue;
+                            }
+                        }
+                        else if (command == "fadelayer")
+                        {
+                            (Scene.SceneManager.currentScene.layers[arguments[0]] as
+                                Scene.Layers.ImageLayer).opacity = 0;
+                        }
+                        else if (command == "additem")
+                        {
+                            Player.Inventory.Items.items[arguments[0]].OnStage = true;
+                            Player.Inventory.Items.items[arguments[0]].initialPosition = new Vector2(
+                                float.Parse(arguments[1], CultureInfo.InvariantCulture),
+                                float.Parse(arguments[2], CultureInfo.InvariantCulture));
+                            Player.Inventory.Items.items[arguments[0]].linearProgress = 0;
+                        }
+                        else if (command == "wait")
+                        {
+                            //Convert the given time which is measured in seconds to milliseconds
+                            System.Threading.Thread.Sleep((int)(float.Parse(arguments[0], CultureInfo.InvariantCulture) * 1000));
+                        }
+                        else if (command == "moveitem")
+                        {
+                            Player.Inventory.Items.items[arguments[0]].currentGoal =
+                                new Vector2(float.Parse(arguments[1], CultureInfo.InvariantCulture),
+                                    float.Parse(arguments[2], CultureInfo.InvariantCulture));
+                            Player.Inventory.Items.items[arguments[0]].linearProgress = 0;
+                            Player.Inventory.Items.items[arguments[0]].itemState = Player.Inventory.Item.ItemStates.ToScene;
+                        }
+                        else if (command == "removeitem")
+                        {
+                            Player.Inventory.Items.items[arguments[0]].OnStage = false;
+                        }
                     }
-                }
-                else if (command == "fadelayer")
-                {
-                    (Scene.SceneManager.currentScene.layers[arguments[0]] as
-                        Scene.Layers.ImageLayer).opacity = 0;
-                }
-                else if (command =="additem")
-                {
-                    Player.Inventory.Items.items[arguments[0]].OnStage = true;
-                    Player.Inventory.Items.items[arguments[0]].initialPosition = new Vector2(
-                        float.Parse(arguments[1], CultureInfo.InvariantCulture),
-                        float.Parse(arguments[2], CultureInfo.InvariantCulture));
-                    Player.Inventory.Items.items[arguments[0]].linearProgress = 0;
-                }
-            }
+                }));
         }
         public static void AddVar(string name, string value)
         {
 
+        }
+        /// <summary>
+        /// Pauses the current htread for the given time
+        /// </summary>
+        /// <param name="ms">The time, measured in milliseconds</param>
+        static void Sleep(int ms)
+        {
+            new System.Threading.ManualResetEvent(false).WaitOne(ms);
         }
     }
 }
